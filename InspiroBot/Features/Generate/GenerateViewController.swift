bc2da393e853f89957323c2e28cdf10270f9c545
Matt Jones <matt.jones@willowtreeapps.com>
2018-08-31 13:29:56 -0400

    Add favoriting from the generation screen

--- InspiroBot/Features/Generate/GenerateViewController.swift
+++ InspiroBot/Features/Generate/GenerateViewController.swift
@@ -10,17 +10,47 @@ import UIKit
 
 class GenerateViewController: UIViewController {
     
-    @IBOutlet var imageView: UIImageView!
-    @IBOutlet var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet private var imageView: UIImageView!
+    @IBOutlet private var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet private var shareButton: UIBarButtonItem!
+    @IBOutlet private var toggleFavoriteButton: UIBarButtonItem!
+    
+    private var poster: Poster?
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        guard let poster = poster else { return }
+        toggleFavoriteButton.title = InspiroBotService.shared.isFavorite(poster) ? "Unfavorite" : "Favorite"
+    }
     
     @IBAction private func generate() {
-        self.imageView.image = nil
+        imageView.image = nil
+        shareButton.isEnabled = false
+        toggleFavoriteButton.isEnabled = false
+        toggleFavoriteButton.title = "Favorite"
         activityIndicator.startAnimating()
         InspiroBotService.shared.generatePoster { poster in
+            self.poster = poster
             ImageLoader(url: poster.url).loadImage { image in
                 self.activityIndicator.stopAnimating()
                 self.imageView.image = image
+                self.shareButton.isEnabled = true
+                self.toggleFavoriteButton.isEnabled = true
             }
+        }
+    }
+    
+    @IBAction private func share() {
+        
+    }
+    
+    @IBAction private func toggleFavorite(_ sender: UIBarButtonItem) {
+        guard let poster = poster else { return }
+        
+        if InspiroBotService.shared.toggleFavorite(poster) {
+            toggleFavoriteButton.title = "Unfavorite"
+        } else {
+            toggleFavoriteButton.title = "Favorite"
         }
     }
 
